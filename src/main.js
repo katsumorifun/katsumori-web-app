9a519b792369c3c60aa10fed3c2f9ec21e5e9184
Daniil <den4ic2001@gmail.com>
2023-02-23 16:07:19 +0300

    added FontAwesome

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import fontawesome from './fontawesome'
 import './assets/main.css'
 import axios from 'axios';
 import lazyLoadingImage from "./directive/lazyLoading/image"
-
 
 axios.defaults.baseURL = 'https://dev.katsumori.fun/api';
 const DEFAULT_TITLE = "Katsumori"
@@ -16,6 +16,7 @@ const app = createApp(App)
 app.use(router)
 app.use(store)
 app.directive('lazy-img', lazyLoadingImage)
+app.component('font-awesome-icon', fontawesome)
 
 router.afterEach((to, from) => {
     document.getElementsByTagName('meta')["description"].content = to.meta.description;
